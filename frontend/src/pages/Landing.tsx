744a7897d89c8257c7559fa415ed417a7b0f9e8a
Marat <marat.karlin@gmail.com>
2025-08-19 11:17:12 +0300

    Update Landing page

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,6 +1,7 @@
 import { motion, AnimatePresence } from "motion/react";
 import { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
+import { RepeatIcon } from "../components/Icons";
 
 const WordSwitcher = (props: any) => {
     const { words, interval, color } = props
@@ -71,13 +72,39 @@ const LandingPage = () => {
                         <button style={{height: 45, width: 250}} className="btn-primary btn-outline" onClick={() => navigate("/dashboard")}>I have an account already</button>
                     </div>
                 </div>
-                <div className="row-landing">
-                    <div className="card landing-demo" 
-                        style={{backgroundColor: "var(--green-color)", border: "2px solid var(--dark-green-color)"}}>
-                        <img className="landing-img" src="/assets/demo-plant.png" alt="Plant (companion) page demo" />
+                <div className="row-landing" style={{alignItems: "center"}}>
+                    <div className="col" style={{gap: 15}}>
+                        <h1 className="right-text" style={{marginBottom: 0, marginTop: 0}}><span style={{fontWeight: "normal"}}>learn</span> essentials</h1>
+                        <p className="right-text">Helfy provides essential information about health that is relevant to regular people, <span style={{fontWeight: "bold"}}>
+                            not</span> athletes, billioners or people with infinite time.</p>
+                        <button className="btn-primary green" onClick={() => navigate("/sign-up")}>Try Helfy</button>
                     </div>
-                    <div className="card landing-demo">
-                        <img className="landing-img" style={{maxWidth: 235}} src="/assets/demo-dashboard.png" alt="Dashboard page demo" />
+                    <div className="col">
+                        <img src="/assets/learnings_demo.png" alt="Learning example" style={{width: 320}}/>
+                    </div>
+                </div>
+                <div className="row-landing" style={{alignItems: "center"}}>
+                    <div className="col">
+                        <div className="card" style={{width: 320, backgroundColor: "var(--yellow-color)", border: "2px solid var(--dark-yellow-color)"}}>
+                            <div className="task-mark icon-row">
+                                <RepeatIcon color="var(--dark-yellow-color)" width="20" height="20" />
+                                <p style={{color: "var(--dark-yellow-color)"}}>Habit</p>
+                            </div>
+                            <p className="sm-heading right-text" style={{marginTop: 10}}>Plate your meals</p>
+                            <p style={{maxWidth: "70%"}} className="right-text">Make sure that your meals are well balanced overall; follow plate method as best as you can.</p>
+                            <div className="row to-right">
+                                <p className="sm-heading">+ 16 xp</p>
+                                <div className="icon-row">
+                                    <p className="sm-heading" style={{color: "var(--dark-yellow-color)"}}>4</p>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill={"var(--dark-yellow-color)"} viewBox="0 0 256 256"><path d="M221.87,90.86a4,4,0,0,0-6.17-.62l-75.42,75.42A8,8,0,0,1,129,154.35l92.7-92.69a8,8,0,0,0-11.32-11.32L197,63.73A112.05,112.05,0,0,0,22.34,189.25,16.09,16.09,0,0,0,37.46,200H218.53a16,16,0,0,0,15.11-10.71,112.28,112.28,0,0,0-11.77-98.43ZM57.44,166.41a8,8,0,0,1-6.25,9.43,7.89,7.89,0,0,1-1.6.16,8,8,0,0,1-7.83-6.41A88.06,88.06,0,0,1,143.59,65.38a8,8,0,0,1-2.82,15.75,72.07,72.07,0,0,0-83.33,85.28Z"></path></svg>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col" style={{gap: 15}}>
+                        <h1 className="right-text" style={{marginBottom: 0, marginTop: 0}}><span style={{fontWeight: "normal"}}>inaction is a</span> slow death</h1>
+                        <p className="right-text">So in Helfy there are Tasks (change every day) and <span style={{color: "var(--dark-yellow-color)"}}>Habits</span> (need to complete them every day).</p>
+                        <button className="btn-primary blue" onClick={() => navigate("/sign-up")}>Try Helfy</button>
                     </div>
                 </div>
                 <div className="col center-align" style={{textAlign: "center"}}>
