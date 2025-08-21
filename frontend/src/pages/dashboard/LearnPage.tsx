51281826a0c8c067689da9a3fd7d71c4c93f801e
Marat <marat.karlin@gmail.com>
2025-08-21 12:41:14 +0300

    fix: Learn page button is not visible

--- frontend/src/pages/dashboard/LearnPage.tsx
+++ frontend/src/pages/dashboard/LearnPage.tsx
@@ -66,9 +66,9 @@ const LearnPage = () => {
                         <p className="right-text">{learnData.tldr}</p>
                     </div>
                 ): (
-                <div style={{maxWidth: 350}} dangerouslySetInnerHTML={{ __html: learnData.body }} />
+                <div style={{maxWidth: 350, marginBottom: 175}} dangerouslySetInnerHTML={{ __html: learnData.body }} />
                 )}
-                <button className="to-bottom btn-primary" onClick={handleContinue}>
+                <button className="to-bottom btn-primary btn-down" onClick={handleContinue}>
                     Continue
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 256 256"><path d="M221.66,133.66l-72,72a8,8,0,0,1-11.32-11.32L196.69,136H40a8,8,0,0,1,0-16H196.69L138.34,61.66a8,8,0,0,1,11.32-11.32l72,72A8,8,0,0,1,221.66,133.66Z"></path></svg>
                 </button>
